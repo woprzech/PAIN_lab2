8766033931564ed55ab7dcd1a77a53dcb1d7d310
RNowak4 <Radek095@gmail.com>
2016-04-10 19:01:46 +0200

    Dodane MDI :joy:

---
+++ PAIN_lab2/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAIN_lab2
+{
+    class Point
+    {
+        private double x, y, z;
+
+        public Point(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void changeX(double x) {
+            this.x = x;
+        }
+
+        public void changeY(double y)
+        {
+            this.y = y;
+        }
+
+        public void changeZ(double z)
+        {
+            this.z = z;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+        public double getZ()
+        {
+            return z;
+        }
+    }
+}
